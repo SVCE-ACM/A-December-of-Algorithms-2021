0308212c13a70f0f190fab11f9d5d59c9f3e499e
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2021-12-23 19:37:44 +0530

    December 05 solution complete

---
+++ December-05/python3_ASHIK11ab.py
@@ -0,0 +1,42 @@
+class Solution:
+  def __init__(self, customers, biscuits):
+    self.customers = customers
+    self.biscuits = biscuits
+
+  def solve(self):
+    # No of biscuits in stack.
+    stack_len = len(self.biscuits)
+    cnt = 0
+    
+    # Complete one round of iteration and if the buiscuits stack 
+    # is changed or not.
+    while cnt <  len(self.customers):
+      # When customers likes the buiscuit.
+      if self.customers[0] == self.biscuits[0]:
+        self.customers = self.customers[1:]
+        self.biscuits = self.biscuits[1:]
+      else:
+        # Customer removed and placed at end of Queue.
+        curr_customer = self.customers[0]
+        self.customers = self.customers[1:]
+        self.customers.append(curr_customer)
+        cnt += 1
+
+    # Stack length not changed indicates that no 
+    # customer in the queue prefers the buiscuit at
+    # the top of the stack.
+    if len(self.biscuits) == stack_len:
+      print(f"Customers that are unable to eat = {len(self.customers)}")
+    else:
+      # Keep solving until no user prefers the buiscuit at top
+      # of the stack or until all biscuits are over.
+      self.solve()
+
+
+def main(customers, biscuits):
+  obj = Solution(customers=customers, biscuits=biscuits)
+  obj.solve()
+
+
+if __name__ == "__main__":
+  main(customers = [1,1,0,0,1,0], biscuits = [0,1,0,1,1,1])
