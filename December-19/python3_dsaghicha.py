ce76a47286ca86f71c7429f1304761c860320a16
DSAghicha <dsaghicha211@gmail.com>
2022-01-10 02:25:56 +0530

    bug: Removed Safe Check

--- December-19/python3_dsaghicha.py
+++ December-19/python3_dsaghicha.py
@@ -3,10 +3,9 @@
 @author: DSAghicha (Darshaan Aghicha)
 @access: public
 """
-def mandatory_courses(course: list[list[int]],answer: list[list[int]]) -> tuple[list[bool], int]:
+def mandatory_courses(course: list[list[int]],answer: list[list[int]]) -> list[bool]:
     result: list[bool] = []
     indirect_dependency: list[int] = []
-    indirect_index: int = -1
     for i in range(len(course)):
         if i + 1 < len(course) and course[i][1] == course[i+1][0]:
             pr: list[int] = [course[i][0], course[i+1][1]]
@@ -14,11 +13,10 @@ def mandatory_courses(course: list[list[int]],answer: list[list[int]]) -> tuple[
 
     for answers in answer:
         if indirect_dependency == answers:
-            indirect_index = answer.index(answers)
             result.append(True)
         else:
             result.append(answers in course)
-    return (result, indirect_index)
+    return result
 
 
 def main() -> None:
@@ -39,15 +37,11 @@ def main() -> None:
             if len(answer) != 2:
                 raise ValueError
             answers.append(answer)
-        output, format_index = mandatory_courses(courses, answers)
+        output = mandatory_courses(courses, answers)
         print(f"Output: {output}")
         print(f"\n{'-' * 10}\n")
         for i in range(len(answers)):
-            if format_index == i:
-                print(format_index)
-                print(f"Course {answers[i][0]} is mandatory to take up course {answers[i][1]}.")
-
-            elif output[i] is True:
+            if output[i] is True:
                 print(f"Course {answers[i][0]} is mandatory to take up course {answers[i][1]}.")
             elif output[i] is False:
                 print(f"Course {answers[i][0]} is not mandatory to take up course {answers[i][1]}.")
